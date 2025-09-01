65937263ca9f1b4c3a607a60e82f40a48611a379
carloslimaeduardo <carloslimaeduardo7@gmail.com>
2025-08-31 21:34:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contactForm');
+    const resposta = document.getElementById('resposta');
+    if(form){
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            resposta.textContent = 'Olá! Obrigado por entrar em contato. Recebi sua mensagem e vou responder o mais rápido possível.';
+            form.reset();
+        });
+    }
+});
